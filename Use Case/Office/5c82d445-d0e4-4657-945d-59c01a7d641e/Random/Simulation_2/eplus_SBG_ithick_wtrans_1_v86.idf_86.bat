dd356d85585a79d8a4e440258486ebbb48859ad0
Daniel Paredes-Soto <->
2021-10-28 00:19:55 +0100

    Removing files

--- Use Case/Office/5c82d445-d0e4-4657-945d-59c01a7d641e/Random/Simulation_2/eplus_SBG_ithick_wtrans_1_v86.idf_86.bat
+++
@@ -1,8 +0,0 @@
-@echo OFF
-cls
-set idffilename=%~n0
-set idffilename=%idffilename:~6,-3%
-echo "+-----------------"
-echo Processing IDF filename: "%idffilename%"
-C:\Users\tuos\Documents\shared-folder\NoMASS-Release-72019\NoMASS-GUI\Parag\EnergyPlus-8.6.0\build\Products\Release\energyplus.exe -w CHE_Geneva.067000_IWEC.epw %idffilename%
-pause
